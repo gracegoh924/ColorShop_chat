2508f12922bdf2d21bb6150f62bbf8481e54d0e8
gracegoh <gracegoh924@gmail.com>
2023-01-12 23:22:32 +0900

    Return dictionary

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Article
 
 
 @api_view(['GET'])
 def index(request):
-    return Response('연결되었습니다')
+    articles = Article.objects.all()
+    # print(articles)
+    article = articles[0]
+    article_dict = {
+        "title":article.title,
+        "content":article.content,
+        "created_at":article.created_at,
+        "updated_at":article.updated_at,
+    }
+    return Response(article_dict)
